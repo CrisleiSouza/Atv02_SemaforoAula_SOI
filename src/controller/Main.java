80833a3f314ee2266e13cb87d40bb9fac687eae4
FATEC ZONA LESTE <FATEC ZONA LESTE@10.68.77.59>
2025-09-11 17:55:58 -0300

    1° Commit

---
+++ src/controller/Main.java
@@ -0,0 +1,12 @@
+package controller;
+
+import java.util.concurrent.Semaphore;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/controller/ThreadAeroporto.java
@@ -0,0 +1,7 @@
+package controller;
+
+import java.util.concurrent.Semaphore;
+
+public class ThreadAeroporto extends Thread{
+	// Vou fazer hoje mesmo professor, não consegui a tempo
+}
